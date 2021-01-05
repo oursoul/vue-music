481749f49a73dc9ac9f5c43a1e0b65b2a532e5c7
oursoul <2335462510@qq.com>
2021-01-05 17:30:22 +0800

    初始化

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    hello,world
+  </div>
+</template>
+
+<script>
+/* eslint-disable */
+export default {
+  
+
+  name: 'App'
+}
+</script>
+
+<style scoped lang='stylus'>
+@import 'common/stylus/variable.styl'
+  #app
+    color:$color-theme;
+    font-size:$font-size-large-x ;
+</style>
+  
+    
+</style>
